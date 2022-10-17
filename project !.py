e45c4aebf04876226bb6d479cf81bd886d0dbe6a
amirreza <amirreza.asgari.tk@gmail.com>
2022-10-17 20:10:24 +0330

    Add files via upload

---
+++ project !.py
@@ -0,0 +1,11 @@
+# limit = int(input("limit your number:"))
+# op = input(' if you want to mathematical division type / or type .: ')
+# death = 0
+# for i in range(limit):
+#     number = int(input("enter number:"))
+#     if op == "/":
+#         death+=number / limit 
+#     elif op == ".":
+#         death+=number
+# print(death)
+
